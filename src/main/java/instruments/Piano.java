a1136f2e90cb22118c52100e3593c1c8fa38b096
Dolf <dmitry.dailidovich@gmail.com>
2016-06-29 23:50:24 +0300

    added instruments

---
+++ src/main/java/instruments/Piano.java
@@ -0,0 +1,11 @@
+package instruments;
+
+public class Piano implements Instrument {
+    public Piano() {
+
+    }
+
+    public void play() {
+        System.out.println("PLINK PLINK PLINK");
+    }
+}
