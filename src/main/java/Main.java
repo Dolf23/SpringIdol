6ac36e5f903a0521aa411c8e56e3f2852293cba3
Dolf <dmitry.dailidovich@gmail.com>
2016-06-29 23:52:01 +0300

    beans added to config

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,14 +2,35 @@ import exceptions.PerformanceException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import performers.Performer;
+import performers.PoeticJuggler;
+import poems.Poem;
+import poems.Sonnet29;
 
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
 
-        try{
+        try {
+            System.out.println("Performance №1");
             Performer performer = (Performer) context.getBean("duke");
             performer.perform();
+            System.out.println();
+
+            System.out.println("Performance №2");
+            Poem sonnet29 = new Sonnet29();
+            Performer duke = new PoeticJuggler(15, sonnet29);
+            duke.perform();
+            System.out.println();
+
+            System.out.println("Performance №3");
+            performer = (Performer) context.getBean("kenny");
+            performer.perform();
+            System.out.println();
+
+            System.out.println("Performance №4");
+            performer = (Performer) context.getBean("kenny2");
+            performer.perform();
+            System.out.println();
         } catch (PerformanceException e) {
             System.out.println(e.getMessage());
         }
